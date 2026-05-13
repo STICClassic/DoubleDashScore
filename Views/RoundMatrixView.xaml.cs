ab46b0c88c83dbfae0ff5020142f404a1d2b049c
Claes Bergenholtz <claes.bergenholtz@veprox.se>
2026-05-13 19:43:33 +0200

    Revert "Skiva 5: Layout — flytta Pickers in i RoundMatrixView som ColumnHeaders"
    This reverts commit 1c38e8115231371ee85dc5556140b7aab38efe9e.

--- Views/RoundMatrixView.xaml.cs
+++ Views/RoundMatrixView.xaml.cs
@@ -16,18 +16,6 @@ public partial class RoundMatrixView : ContentView
         defaultValue: "16",
         defaultBindingMode: BindingMode.TwoWay);
 
-    public static readonly BindableProperty ColumnHeader0Property = BindableProperty.Create(
-        nameof(ColumnHeader0), typeof(View), typeof(RoundMatrixView));
-
-    public static readonly BindableProperty ColumnHeader1Property = BindableProperty.Create(
-        nameof(ColumnHeader1), typeof(View), typeof(RoundMatrixView));
-
-    public static readonly BindableProperty ColumnHeader2Property = BindableProperty.Create(
-        nameof(ColumnHeader2), typeof(View), typeof(RoundMatrixView));
-
-    public static readonly BindableProperty ColumnHeader3Property = BindableProperty.Create(
-        nameof(ColumnHeader3), typeof(View), typeof(RoundMatrixView));
-
     public IReadOnlyList<PlayerColumnViewModel>? Players
     {
         get => (IReadOnlyList<PlayerColumnViewModel>?)GetValue(PlayersProperty);
@@ -38,30 +26,6 @@ public partial class RoundMatrixView : ContentView
     {
         get => (string)GetValue(TrackCountTextProperty);
         set => SetValue(TrackCountTextProperty, value);
-    }
-
-    public View? ColumnHeader0
-    {
-        get => (View?)GetValue(ColumnHeader0Property);
-        set => SetValue(ColumnHeader0Property, value);
-    }
-
-    public View? ColumnHeader1
-    {
-        get => (View?)GetValue(ColumnHeader1Property);
-        set => SetValue(ColumnHeader1Property, value);
-    }
-
-    public View? ColumnHeader2
-    {
-        get => (View?)GetValue(ColumnHeader2Property);
-        set => SetValue(ColumnHeader2Property, value);
-    }
-
-    public View? ColumnHeader3
-    {
-        get => (View?)GetValue(ColumnHeader3Property);
-        set => SetValue(ColumnHeader3Property, value);
     }
 
     public RoundMatrixView()
